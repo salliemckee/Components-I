6065aaf504b85c8f66fe97012728cbe27b8d7c67
Sallie McKee <salliemckee@Morrigan.local>
2022-02-09 16:19:13 -0600

    finished article.js portion

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -88,6 +88,13 @@ const data = [
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`,
   },
+  {
+    title: "Really Hoping this Article Shows Up",
+    date: "February 9, 2022",
+    firstParagraph: `Do try and keep out of my way in future and in past, there’s a good fellow. The time continuum should be big enough for the both of us. The least important things, sometimes my dear boy, lead to the greatest discoveries. Great men are forged in fire. It is the privilege of lesser men to light the flame. Whatever the cost. Why can’t you leave me alone?! Allow me to congratulate you, sir. You have the most totally closed mind that I’ve ever encountered. Butterfingers. Allons-y! When I say run, run. …RUN!`,
+    secondParagraph: `A TARDIS is my ship that carries me through Time and Space. T-A-R-D-I-S. It stands for Time And Relative Dimension In Space. A little gratitude wouldn’t irretrievably damage my ego. Our lives are different from anybody else’s. That’s the exciting thing. Nobody in the universe can do what we’re doing. Let’s go in! I am the Doctor! The original you might say! Would you like a jelly baby? Boring. And I hate hotels. It was on the planet Skaro that my old enemy the Master was finally put on trial. They say he listened calmly as his list of evil crimes was read… and sentence passed. Then he made his last and I thought somewhat curious request. He demanded that I, the Doctor, a rival Timelord, should take his remains back to our home planet, Gallifrey.`,
+    thirdParagraph: `I don’t know. But I’d probably blame the English. Well, it wouldn’t be cricket. It’s a fez. I wear a fez now. Fezzes are cool. So these are my replacements. A dandy and a clown. This is a situation that requires tact and finesse. Fortunately, I am blessed with both. People spend all their time making nice things and then other people come along and break them! Jelly baby? The trouble with computers, of course, is that they’re very sophisticated idiots. They do exactly what you tell them at amazing speed. Even if you order them to kill you. So if you do happen to change your mind, it’s very difficult to stop them from obeying the original order. But not impossible.`,
+  },
 ];
 
 /*
@@ -115,6 +122,7 @@ const data = [
   Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
   Refresh the page to see the new article.
 */
+const divArticle = document.querySelector("div.articles");
 
 function articleMaker(articleObj) {
   const article = document.createElement("div");
@@ -147,3 +155,18 @@ function articleMaker(articleObj) {
   });
   return article;
 }
+
+//loop over data//
+
+const articleElements = data.map((articleObj) => {
+  return articleMaker(articleObj);
+});
+
+articleElements.forEach((articleElem) => {
+  divArticle.appendChild(articleElem);
+});
+
+data.forEach((articleObj) => {
+  const dataElem = articleMaker(articleObj);
+  divArticle.appendChild(dataElem);
+});
